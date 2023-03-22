ff0004c9cd9d37412adf2eec97d61640bd3c1d14
diegoSisalima <tudiego007@hotmail.com>
2023-03-21 19:29:40 -0500

    style: footer and header complete with state login true or false

--- components/navigation/footer/Footer.tsx
+++ components/navigation/footer/Footer.tsx
@@ -1,7 +1,11 @@
+import Input from '../../input/Input';
+
 export function Footer() {
   return (
-    <div className="h-[483px] bg-[url('/footer-banner.png')] bg-center bg-no-repeat bg-cover lg:h-[488px]">
-      Hola como estas
+    <div className="h-[483px] bg-[url('/footer-banner.png')] bg-center bg-no-repeat bg-cover lg:h-[488px] relative">
+      <div className="absolute bottom-[137px] -translate-x-2/4 left-2/4 w-[362px] sm:w-[465px]">
+        <Input />
+      </div>
     </div>
   );
 }
