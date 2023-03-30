f2ded1b098afb7ceed85dcdc04152f1fd94fa132
diegoSisalima <tudiego007@hotmail.com>
2023-03-30 14:59:58 -0500

    style: add section recientes

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,8 +1,19 @@
 import { Layout } from '../../components/layout/Layout';
+import { EventSlider } from '../../components/sliders/EventSlider/EventSlider';
+import { eventsMock } from '../../lib/data/events.mock';
 import { NextPageWithLayout } from '../page';
 
 export const SearchPage: NextPageWithLayout = () => {
-  return <div>Search</div>;
+  return (
+    <div>
+      Search
+      <EventSlider
+        title="Recientes"
+        subtitle="Las personas últimanete están hablando de esto"
+        events={eventsMock}
+      />
+    </div>
+  );
 };
 SearchPage.getLayout = (page) => {
   return <Layout>{page}</Layout>;
