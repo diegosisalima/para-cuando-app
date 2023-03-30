6de57ce1be8fd51075bbca1a4ecf77d370313af0
diegoSisalima <tudiego007@hotmail.com>
2023-03-30 11:47:42 -0500

    style: page detail

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -27,38 +27,7 @@ export const ProfilePage: NextPageWithLayout = () => {
       </section>
       <section className="pt-16 flex flex-col items-center gap-8 sm:flex-row sm:flex-wrap sm:justify-center">
         <EventCard
-          title="Tienda Zara"
-          short_description="Tienda por departamento de marca de ropa y
-accesorios."
-          votes={1345}
-          url="tiendazara.com"
-          image="/mock-event-image.png"
-        />
-        <EventCard
-          title="Tienda Zara"
-          short_description="Tienda por departamento de marca de ropa y
-accesorios."
-          votes={1345}
-          url="tiendazara.com"
-          image="/mock-event-image.png"
-        />
-        <EventCard
-          title="Tienda Zara"
-          short_description="Tienda por departamento de marca de ropa y
-accesorios."
-          votes={1345}
-          url="tiendazara.com"
-          image="/mock-event-image.png"
-        />
-        <EventCard
-          title="Tienda Zara"
-          short_description="Tienda por departamento de marca de ropa y
-accesorios."
-          votes={1345}
-          url="tiendazara.com"
-          image="/mock-event-image.png"
-        />
-        <EventCard
+          id={2}
           title="Tienda Zara"
           short_description="Tienda por departamento de marca de ropa y
 accesorios."
@@ -67,7 +36,7 @@ accesorios."
           image="/mock-event-image.png"
         />
       </section>
-      <div className="w-full text-center pt-20 pb-16">
+      <div className="w-32 mx-auto pt-20 pb-16">
         <BtnBlue text="Ver más" />
       </div>
     </div>
